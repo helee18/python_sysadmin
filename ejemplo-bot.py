660d2aedc9d2bcba4a2e30c2d2e12ac33cd9e8a5
helee18 <helena.gutierrez.003@gmail.com>
2020-05-18 17:53:12 +0200

    ejemplo basico

---
+++ ejemplo-bot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Importamos los modulos y submodulos necesarios
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Habilitamos el registro
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Definimos las funciones
+def start(update, context):
+    update.message.reply_text(
+        'Welcome to PythonSysadminBot ' + update.message.from_user.first_name
+    )
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def main():
+    # Introducimos nuestro Token
+    updater = Updater('1166829225:AAGl0qJmYnUggGmNvX4RU2a8BQyD1u5FCmE', use_context=True)
+
+    # Definimos los comandos y las funciones a ejecutar
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+
+    # Log para errores
+    updater.dispatcher.add_error_handler(error)
+
+    # Cuando no entiende el comando de entrada
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+    # Iniciamos el bot y configuramos que esté pendiente y a la espera de mensajes
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
