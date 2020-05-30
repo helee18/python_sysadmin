7ac550d49f457ac02a69e7ba8e6c6e018d8e6e4a
helee18 <helena.gutierrez.003@gmail.com>
2020-05-30 19:11:28 +0200

    terminal fuera

--- bot.py
+++ bot.py
@@ -4,29 +4,13 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from auth.auth import token
-import os
+from comandos_linux import terminal
 
 # Habilitamos el registro
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
-# Funcion ejecutar comandos Linux
-
-def terminal(entrada):
-    salida = ''
-    # Ejecutamos el comando en el terminal
-    f = os.popen(entrada)
-
-    # Leemos caracter a caracter y lo guardamos en la variable a devolver
-    for i in f.readlines():
-        salida += i 
-    # Eliminamos el salto de linea
-    salida = salida[:-1]
- 
-    # Devolvemos la variable con la respuesta al comando
-    return salida 
 
 # Función para errores
 
