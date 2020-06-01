5b976f8eab4236940dc7c6e93d3766d0d7fdb21b
helee18 <helena.gutierrez.003@gmail.com>
2020-06-01 02:32:00 +0200

    ids

--- ejemplo-bot.py
+++ ejemplo-bot.py
@@ -3,7 +3,7 @@
 # Importamos los modulos y submodulos necesarios
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from auth.auth import token
+from auth import token, ids
 
 # Habilitamos el registro
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,17 +11,45 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-# Definimos las funciones
-def start(update, context):
-    update.message.reply_text(
-        'Welcome to PythonSysadminBot ' + update.message.from_user.first_name
-    )
+# Función para errores
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+# Función de repetición 
+
 def echo(update, context):
     update.message.reply_text(update.message.text)
+
+# Funciones para comandos
+
+def start(update, context):
+    # Comprobamos si es un usuario autorizado
+    if update.message.chat_id in ids:
+        # Confirmamos que se ha iniciado bien con un mensaje de bienvenida
+        update.message.reply_text(
+            'Welcome to PythonSysadminBot ' + update.message.from_user.first_name
+        )
+    else:
+        # Si no es un usuario autorizado
+        update.message.reply_text(
+            'No perteneces a los usuarios autorizados'
+        )
+
+def help(update,context):
+    # Comprobamos si es un usuario autorizado
+    if update.message.chat_id in ids:
+        # Listamos todos los comandos
+        update.message.reply_text(
+            '*Lista de comandos* \n\n'
+            '/start - inicio del bot \n', 
+            parse_mode= 'Markdown'
+        )
+    else:
+        # Si no es un usuario autorizado
+        update.message.reply_text(
+            'No perteneces a los usuarios autorizados'
+        )
 
 def main():
     # Introducimos nuestro Token
@@ -29,6 +57,7 @@ def main():
 
     # Definimos los comandos y las funciones a ejecutar
     updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
 
     # Log para errores
     updater.dispatcher.add_error_handler(error)
